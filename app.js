20e9073a1fcf9e9bc2cf5d16527b806d0cee14c6
Moaad <moaadagdali2005@gmail.com>
2025-02-20 22:28:33 +0100

    lis

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let boton = document.getElementById("add");
-    let lista = document.getElementById("task-list");
-    let inputTexto = document.getElementById("texto");
+let boton = document.getElementById("add");
+let lista = document.querySelector("ul");
+let inputTexto = document.getElementById("texto");
 
-    function addTask() {
-        if (inputTexto.value.trim() !== "") { 
-            let nuevoItem = document.createElement("li");
-            nuevoItem.className = "list-group-item d-flex align-items-center";
-            nuevoItem.style.listStyleType = "disc"; 
-            
-            let btnEliminar = document.createElement("button");
-            btnEliminar.className = "btn btn-primary btn-sm me-3";
-            btnEliminar.textContent = "Borrar";
-            
-            btnEliminar.addEventListener("click", function() {
-                lista.removeChild(nuevoItem);
-                alert("Borrar Tarea: " + textoTarea.textContent);
-            });
-            
-            let textoTarea = document.createElement("span");
-            textoTarea.textContent = inputTexto.value;
-            nuevoItem.appendChild(btnEliminar);
-            nuevoItem.appendChild(textoTarea);
-            lista.appendChild(nuevoItem);
-
-            inputTexto.value = "";
-            inputTexto.focus();
-        }
-    }
-    boton.addEventListener("click", addTask);
+function addTask()  {
+    if (inputTexto.value !== ""){
+        let nuevoItem = document.createElement("li");
+        nuevoItem.setAttribute("class", "my-3");
+        nuevoItem.textContent= inputTexto.value;
+        lista.appendChild(nuevoItem);
+        addButtonBorrar(nuevoItem);
+    } 
+function addButtonBorrar(nuevoItem){
+    let buttonborrar = document.createElement("button");
+    buttonborrar.textContent = "Borrar";
+    buttonborrar.setAttribute("class", "btn btn-primary float-start mx-4");
+    console.log(buttonborrar);
     
-    inputTexto.addEventListener("keypress", function (e) {
-        if (e.key === "Enter") {
-            addTask();
-        }
+    buttonborrar.addEventListener("click", function() {
+        console.log(nuevoItem);
+        console.log(""+ nuevoItem.textContent);
+        nuevoItem.remove();
     });
+
+    nuevoItem.appendChild(buttonborrar);
+;}
+
+};
+boton.addEventListener("click", function() {
+    addTask()
+    
+});
+inputTexto.addEventListener('keypress', function (e){
+    console.log(e.key);
+    if(e.key === 'Enter'){
+        addTask()
+    }
+
 });
